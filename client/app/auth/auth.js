bdd676cbbe8504d2cd5f0af69bb1d7632c3036da
Ryan Chen <Ryan@Ryans-MacBook-Pro-3.local>
2015-09-24 11:34:55 -0700

    client Authorization 01

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -6,28 +6,21 @@
     $scope.user = {};
 
     $scope.signin = function () {
-      Auth.signin($scope.user)
-        .then(function (token) {
-          $window.localStorage.setItem('com.shortly', token);
-          $location.path('/links');
+      Auth.signin($scope.user, function (token) {
+          $window.localStorage.setItem('com.vacaypay', token);
+          $location.path('/home');
           $scope.signinForm.$setPristine();
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
+      });
     };
 
     $scope.signup = function () {
-      Auth.signup($scope.user)
-        .then(function (token) {
-          $window.localStorage.setItem('com.shortly', token);
-          $location.path('/links');
-          $scope.signupForm.$setPristine();
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
+      Auth.signup($scope.user, function (token) {
+          $window.localStorage.setItem('com.vacaypay', token);
+          $location.path('/home');
+          $scope.signupForm.$setPristine();   
+      });
     };
+
   });
 
 
