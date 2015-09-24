821ee679d74d113c358fc04f58e257f0b719d94a
Ryan Chen <Ryan@Ryans-MacBook-Pro-3.local>
2015-09-24 10:44:26 -0700

    Initial Authorization setup

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,1 +1,38 @@
-angular.module('app')
+(function() {
+  'use strict';
+
+  angular.module('app')
+  .controller('AuthController', function ($scope, $window, $location, Auth) {
+    $scope.user = {};
+
+    $scope.signin = function () {
+      Auth.signin($scope.user)
+        .then(function (token) {
+          $window.localStorage.setItem('com.shortly', token);
+          $location.path('/links');
+          $scope.signinForm.$setPristine();
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    };
+
+    $scope.signup = function () {
+      Auth.signup($scope.user)
+        .then(function (token) {
+          $window.localStorage.setItem('com.shortly', token);
+          $location.path('/links');
+          $scope.signupForm.$setPristine();
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    };
+  });
+
+
+
+
+
+})();
+
