206d76c581c76428a18b3993ab02873d194df1c1
Ryan Chen <Ryan@Ryans-MacBook-Pro-3.local>
2015-09-25 12:26:10 -0700

    feat/currentTrip initial commit

--- client/app/currentTrip/currentTrip.js
+++ client/app/currentTrip/currentTrip.js
@@ -2,8 +2,20 @@
   'use strict';
 
   angular.module('app')
-  .controller('CurrentTripController', function ($scope, Trip) {
+  .controller('CurrentTripController', function ($scope, $modal, $location, Trip) {
     $scope.currentTrip = {};
+
+    $scope.hasTrip = function () {
+      Trip.hasTrip( function (data) {
+        $scope.data = data;
+
+        if (!$scope.data.name) {
+          $location.path('/fallback');
+        }
+      });
+    }
+
+
   })
   
 })();
