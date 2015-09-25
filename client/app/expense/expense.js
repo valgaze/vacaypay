be9ea512e40562b6408646296db28ca57b34aedd
Tomio Mizoroki <tmizoroki@gmail.com>
2015-09-24 17:31:01 -0700

    Build ExpenseController and CollapseDemoCtrl

--- client/app/expense/expense.js
+++ client/app/expense/expense.js
@@ -2,6 +2,23 @@
   'use strict';
 
   angular.module('app')
+  .controller('ExpenseController', function ($scope, Expenses) {
+    $scope.data = {};
 
+    $scope.getExpenses = function() {
+      Expenses.allExpenses()
+      .then(function (expenses) {
+        $scope.data.expenses = expenses;
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+    };
+    $scope.getExpenses();
+  })
+  
+  .controller('CollapseDemoCtrl', function ($scope) {
+  $scope.isCollapsed = false;
+  });
 
 })();
