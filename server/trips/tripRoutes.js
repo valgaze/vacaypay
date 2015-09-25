28b7988f8b50e9cb37efdc31e65f4e2500ed220d
Eugene Lee <colemakdvorak@gmail.com>
2015-09-24 19:37:18 -0700

    Added comments

--- server/trips/tripRoutes.js
+++ server/trips/tripRoutes.js
@@ -3,15 +3,10 @@ var tripController = require('./tripController.js');
 module.exports = function (app) {
 
 app.get('/', tripController.getCurrentTrip);
-
 app.post('/', tripController.createTrip);
-
 app.post('/join', tripController.joinTrip);
-
 app.post('/expense', tripController.addExpense);
-
 app.post('/end', tripController.endTrip);
-
 app.get('/recent', tripController.getRecent);
 
 };
