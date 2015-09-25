ef2e18681af34e4e60591cb9ffe8fa6b5e714a9c
John Anderson <jfanderson373@gmail.com>
2015-09-25 11:23:07 -0700

    Remove express boilerplate code

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var User = require('./users/userModel.js');
 
 var app = express();
 
@@ -8,35 +7,6 @@ mongoose.connect('mongodb://localhost/test');
 
 // routing handled in middleware
 require('./config/middleware.js')(app, express);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
 
 app.listen(3000);
 console.log('Listening to port 3000...');
