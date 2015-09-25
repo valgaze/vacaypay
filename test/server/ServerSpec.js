7f86e93940e714698f2171a703f6869b91947f00
John Anderson <jfanderson373@gmail.com>
2015-09-24 18:43:52 -0700

    Tests for mongo model creation

--- test/server/ServerSpec.js
+++ test/server/ServerSpec.js
@@ -4,8 +4,9 @@ var express = require('express');
 
 var app = require('../../server/app.js');
 var User = require('../../server/users/userModel.js');
+var Trip = require('../../server/trips/tripModel.js');
 
-describe('MongoTests', function() {
+describe('Mongo', function() {
 
   beforeEach(function(done) {
     var newUser = {
@@ -13,27 +14,84 @@ describe('MongoTests', function() {
       password: '1234'
     };
 
-    User.create({ username: 'test' }, function(error) {
+    User.create(newUser, function(error, user) {
       if (error) {
-        console.log('Error creating user: ', error);
+        console.log('Error creating test user: ', error);
       }
-      done();
+  
+      var newTrip = {
+        creator: user._id,
+        participants: [user._id],
+        name: 'test trip',
+        code: 'ABCD',
+        expenses: {
+          name: 'food',
+          amount: 15.50,
+          payer: user._id,
+          stakeholders: [user._id]
+        }
+      };
+
+      Trip.create(newTrip, function(error) {
+        if (error) {
+          console.log('Error creating trip: ', error);
+        }
+        done();
+      });
     });
+
   });
 
   afterEach(function(done) {
     User.remove({ username: 'test' }, function(error) {
       if (error) {
-        console.log('Error removing user: ', error);
+        console.log('Error removing test user: ', error);
       }
-      done();
+  
+      Trip.remove({ name: 'test trip' }, function(error) {
+        if (error) {
+          console.log('Error removing test trip: ', error);
+        }
+        done();
+      });
     });
+
   });
 
-  describe('User creation:', function() {
+  describe('Model creation:', function() {
     
+    it('Should create a new user', function(done) {
+      User.findOne({ username: 'test' })
+        .exec(function(error, user) {
+          if (error) {
+            console.log('Cannot find user: ', error);
+          }
+          
+          expect(user.username).to.equal('test');
+          expect(user.password).to.not.equal('1234');
+          done();
+        });
+    });
 
+    it('Should successfully compare original password to hash', function(done) {
+      User.findOne({ username: 'test' })
+        .exec(function(error, user) {
+          expect(user.comparePasswords('1234')).to.be.true;
+        });
+    });
 
-  });
+    it('Should create a new trip', function(done) {
+      Trip.findOne({ name: 'test trip' })
+        .exec(function(error, trip) {
+          if (error) {
+            console.log('Cannot find trip: ', error);
+          }
+
+          expect(trip.code).to.equal('ABCD');
+          expect(trip.expenses.food).to.equal('food');
+        });
+    });
+
+  }); // model creation
 
 });
