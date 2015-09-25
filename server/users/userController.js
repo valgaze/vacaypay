e8f8e7e01535db4db0fe59504dd0f2cc6369fc3d
John Anderson <jfanderson373@gmail.com>
2015-09-24 19:37:06 -0700

    Add user pathways for client requests to server

--- server/users/userController.js
+++ server/users/userController.js
@@ -1,1 +1,11 @@
 // Controller for route handling user
+
+module.exports = {
+
+  // Check if username exists; if not, create new user
+  signup: function() {},
+
+  // Check if user exists, then compare password to hash
+  signin: function() {}
+
+};
