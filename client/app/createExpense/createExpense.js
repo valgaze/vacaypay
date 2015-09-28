9f6459753c9fbed44ad353d41231c5dc5c8eea49
Tomio Mizoroki <tmizoroki@gmail.com>
2015-09-28 01:39:43 -0700

    Build controller for createExpense modal

--- client/app/createExpense/createExpense.js
+++ client/app/createExpense/createExpense.js
@@ -2,16 +2,40 @@
   'use strict';
 
   angular.module('app')
-  .controller('CreateExpenseController', function ($scope, $modalInstance, $state, Expense) {
+  .controller('CreateExpenseController', function ($scope, $modalInstance, $state, Expenses, Trip) {
     $scope.expense = {};
+    $scope.participants;
+    $scope.stakeholders = [];
+
+    $scope.dropdownSettings = {
+      smartButtonMaxItems: 5,
+      scrollableHeight: '100px',
+      scrollable: true
+    };
+
+    $scope.buttonText = {
+      buttonDefaultText: 'Select Contributors'
+    };
+
     $scope.addExpense = function() {
       console.log($scope.expense);
-      Expense.createExpense($scope.expense, function() {
+      Expenses.addExpense($scope.expense, function() {
         console.log('called back!');
         $modalInstance.dismiss('Expense has been added');
         $state.transitionTo('currentTrip');
       });
     };
+
+    $scope.getParticipants = function() {
+      Trip.hasTrip()
+      .then(function (trip) {
+        $scope.participants = trip.participants;
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+    };
+
 
   });
   
