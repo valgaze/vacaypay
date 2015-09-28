46e8fc1fd03b81f050244634b132127896b5393a
Tomio Mizoroki <tmizoroki@gmail.com>
2015-09-27 17:35:57 -0700

    Build controller for create expense form

--- client/app/createExpense/createExpense.js
+++ client/app/createExpense/createExpense.js
@@ -2,5 +2,17 @@
   'use strict';
 
   angular.module('app')
+  .controller('CreateExpenseController', function ($scope, $modalInstance, $state, Expense) {
+    $scope.expense = {};
+    $scope.addExpense = function() {
+      console.log($scope.expense);
+      Expense.createExpense($scope.expense, function() {
+        console.log('called back!');
+        $modalInstance.dismiss('Expense has been added');
+        $state.transitionTo('currentTrip');
+      });
+    };
+
+  });
   
 })();
