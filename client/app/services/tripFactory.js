e27f07c0799ee9af1e81e2bb0a66fd9def1d1e43
Tomio Mizoroki <tmizoroki@gmail.com>
2015-09-30 15:12:33 -0700

    Add getTrip function in Trip factory that retrieves and caches the trip data.

--- client/app/services/tripFactory.js
+++ client/app/services/tripFactory.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
   angular.module('app')
-  .factory('Trip', function ($http) {
+  .factory('Trip', function ($http, $cacheFactory, Auth) {
 
     var services = {
       createTrip: createTrip,
@@ -14,6 +14,7 @@
     function createTrip(trip, callback) {
       console.log(trip);
       $http.post('/trips', {
+        user: Auth.currentUser(),
         name: trip.name,
         code: trip.code
       })
@@ -23,8 +24,20 @@
       });
     }
 
-    function getTrip() {
-      $http.get('/trips', { cache: true });
+    function getTrip(callback) {
+      $http.get('/trips', {
+        params: {user: Auth.currentUser()}
+      })
+      .then(function (res) {
+        var cache = $cacheFactory('tripData');
+        cache.put('_id', res.data[0]._id);
+        cache.put('creator', res.data[0].creator);
+        cache.put('participants', res.data[0].participants);
+        cache.put('name', res.data[0].name);
+        cache.put('code', res.data[0].code);
+        cache.put('expenses', res.data[0].expenses);
+        callback(cache);
+      });
     }
 
     function hasTrip(callback) {
