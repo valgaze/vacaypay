52b5ebebd1ed33c6d759b0c60681ecc678435e71
Tomio Mizoroki <tmizoroki@gmail.com>
2015-09-30 21:40:31 -0700

    Build preliminary layout and logic for pastTrips

---
+++ client/app/services/pastTripFactory.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+
+  angular.module('app')
+ .factory('PastTrips', function ($http) {
+    var services = {
+      allPastTrips: allPastTrips
+    };
+
+    return services;
+
+    function allPastTrips (callback) {
+      $http.get('/trips') // We don't have a route yet?
+      .then(function (res) {
+        callback(res.data);
+      });
+    }
+
+  });
+})();
