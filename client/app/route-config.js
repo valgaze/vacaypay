a79c97f614b1f474fc4fd77409846fd48af04cf3
Ryan Chen <Ryan@Ryans-MacBook-Pro-3.local>
2015-10-01 18:58:10 -0700

    (feat) Add authentication to routing

--- client/app/route-config.js
+++ client/app/route-config.js
@@ -11,33 +11,39 @@
       .state('signin', {
         url: '/signin',
         templateUrl: 'app/auth/signin.html',
-        controller: 'AuthController'
+        controller: 'AuthController',
+        authenticate: false
       })
       .state('signup', {
         url: '/signup',
         templateUrl: 'app/auth/signup.html',
-        controller: 'AuthController'
+        controller: 'AuthController',
+        authenticate: false
       })
       .state('fallback', {
         url: '/fallback',
         templateUrl: 'app/fallback/fallback.html',
-        controller: 'FallbackController'
+        controller: 'FallbackController',
+        authenticate: true
       })
       .state('currentTrip', {
         url: '/currentTrip',
         abstract: true,
         templateUrl: 'app/currentTrip/currentTrip.html',
-        controller: 'CurrentTripController'
+        controller: 'CurrentTripController',
+        authenticate: true
       })
       .state('currentTrip.expense', {
         url: '',
         templateUrl: 'app/expense/expense.html',
-        controller: 'ExpenseController'
+        controller: 'ExpenseController',
+        authenticate: true
       })
       .state('pastTrips', {
         url: '/pastTrips',
         templateUrl: 'app/pastTrip/pastTrip.html',
-        controller: 'PastTripController'
+        controller: 'PastTripController',
+        authenticate: true
       });
   // We add our $httpInterceptor into the array
   // of interceptors. Think of it like middleware for your ajax calls
@@ -60,7 +66,7 @@
     };
     return attach;
   })
-  .run(function ($rootScope, $location, Auth) {
+  .run(function ($rootScope, $state, Auth) {
     // here inside the run phase of angular, our services and controllers
     // have just been registered and our app is ready
     // however, we want to make sure the user is authorized
@@ -68,9 +74,10 @@
     // when it does change routes, we then look for the token in localstorage
     // and send that token to the server to see if it is a real user or hasn't expired
     // if it's not valid, we then redirect back to signin/signup
-    $rootScope.$on('$routeChangeStart', function (evt, next, current) {
-      if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
-        $location.path('/signin');
+    $rootScope.$on('$stateChangeStart', function (evt, toState) {
+      if (toState.authenticate && !Auth.isAuth()) {
+        console.log('test');
+        $state.transitionTo("signin");
       }
     });
   });
