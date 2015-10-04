5f3e44c291df43394b05c16f027c5cff4d890c72
John Anderson <jfanderson373@gmail.com>
2015-10-04 01:13:36 -0700

    Add concat, uglify, and cssmin tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,46 @@ module.exports = function(grunt) {
         }
       },
 
+      concat: {
+        client: {
+          src: [
+            'client/app/**/*.js'
+          ],
+          dest: 'client/dist/client.js'
+        },
+        dependencies: {
+          src: [
+            'client/lib/jquery/dist/jquery.min.js',
+            'client/lib/bootstrap/dist/js/bootstrap.min.js',
+            'client/lib/angular/angular.js',
+            'client/lib/angular-ui-router/release/angular-ui-router.min.js',
+            'client/lib/angular-bootstrap/ui-bootstrap.min.js',
+            'client/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+            'client/lib/lodash/dist/lodash.min.js',
+            'client/lib/angularjs-dropdown-multiselect/dist/angularjs-dropdown-multiselect.min.js'
+          ],
+          dest: 'client/dist/dependencies.min.js'
+        }
+      },
+
+      uglify: {
+        client: {
+          src: [
+            'client/dist/client.js'
+          ],
+          dest: 'client/dist/client.min.js'
+        }
+      },
+
+      cssmin: {
+        target: {
+          src: [
+            'client/styles/styles.css'
+          ],
+          dest: 'client/dist/styles.min.css'
+        }
+      },
+
       nodemon: {
         dev: {
           script: 'server/app.js'
@@ -20,7 +60,16 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-nodemon');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     
+    grunt.registerTask('build', [
+      'concat',
+      'uglify',
+      'cssmin'
+    ]);
+
     grunt.registerTask('default', [
       'nodemon'
     ]);
