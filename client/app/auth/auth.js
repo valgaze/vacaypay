d35fbe1ea6bcc50bad9919705c46986e23431635
Andrew Gonzales <andrewmartingonzales@gmail.com>
2015-10-06 16:20:30 -0700

    (feat) Displays error on page when signin fails

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -5,14 +5,18 @@
   .controller('AuthController', ['$scope', '$window', '$state', 'Auth', 
   function ($scope, $window, $state, Auth) {
     $scope.user = {};
+    $scope.signinFail = false;
+
     //Sign in function and store token into local storage
     //then change state to currentTrip.html
     $scope.signin = function () {
+
       Auth.signin($scope.user, function (token) {
           $window.localStorage.setItem('com.vacaypay', token);
           $state.transitionTo('currentTrip.expense');
           $scope.signinForm.$setPristine();
       });
+      $scope.signinFail = !(Auth.isAuth());
     };
     //Sign up function and store token into local storage
     //then change state to currentTrip.html
@@ -20,8 +24,10 @@
       Auth.signup($scope.user, function (token) {
           $window.localStorage.setItem('com.vacaypay', token);
           $state.transitionTo('currentTrip.expense');
-          $scope.signupForm.$setPristine();   
+          $scope.signupForm.$setPristine();
       });
+      $scope.signinFail = !(Auth.isAuth());
+
     };
   }]);
 })();
